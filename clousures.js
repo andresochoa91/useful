7ecf9f8036b9644746be1e53dd49a26c4e057b2e
andresochoa91 <andres_ochoa91@hotmail.com>
2020-08-14 01:46:47 -0700

    FP added

--- clousures.js
+++ clousures.js
@@ -7,6 +7,11 @@
 const multiplyBy = a => b => a * b; //Same than above
 console.log(multiplyBy(10)(20));
 
+//Using Partials
+const multiply = (a, b, c) => a * b * c; //Same than above
+const partialMultiplyBy5 = multiply.bind(null, 5);
+console.log(partialMultiplyBy5(5, 6)) 
+
 const multiplyByFive = multiplyBy(5);
 console.log(multiplyByFive(10));
 
@@ -31,3 +36,53 @@ greeting("Hello")("Andres")("Ochoa");
 const yay = () => name => age => `${name} ${age}`;
 const yay2 = yay();
 console.log(yay2("John")("Doe"));
+
+
+//Memoization
+const check = () => {
+  const memo = {};
+  return (number) => {
+    if (!(number in memo)) {
+      console.log("long")
+      memo[number] = true
+    } else {
+      console.log("short")
+    }
+  } 
+}
+
+let checkFun = check();
+
+checkFun(5)
+checkFun(5)
+checkFun(5)
+checkFun(5)
+checkFun(5)
+
+////Encapsulation. This code works in the browser
+
+// const makeNuclearButton = () => {
+//   let timeWithoutDestruction = 0;
+//   const passTime = () => timeWithoutDestruction++;
+//   const totalPeaceTime = () => timeWithoutDestruction;
+//   const launch = () => {
+//     timeWithoutDestruction = -1;
+//     return "boom";
+//   }
+//   setInterval(passTime, 1000);
+//   return { 
+//     totalPeaceTime: totalPeaceTime,
+//     launch: launch
+//   }
+// }
+
+// const ohno = makeNuclearButton();
+// console.log(ohno.launch());
+
+let arr = [1, 2, 3, 4, 5];
+
+for (var i = 0; i < arr.length; i++) {
+  ((i) => setTimeout(() => {
+    console.log("I'm at index " + i);
+  }, 3000))(i);
+}
